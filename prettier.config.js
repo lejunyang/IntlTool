251a27c85e797d6151cdebbcc85f965086c0f297
lejunyang <junyang.le@hand-china.com>
2022-05-16 19:30:41 +0800

    fix: eslint，prettier，ts配置更新

--- prettier.config.js
+++ prettier.config.js
@@ -1,5 +1,14 @@
+/*
+ * @Author: junyang.le@hand-china.com
+ * @Date: 2022-01-20 10:11:01
+ * @LastEditTime: 2022-01-29 11:15:28
+ * @LastEditors: junyang.le@hand-china.com
+ * @Description: your description
+ * @FilePath: \tool\prettier.config.js
+ */
 module.exports = {
   semi: true,
   singleQuote: true,
   arrowParens: 'avoid',
-}
+  printWidth: 120,
+};
