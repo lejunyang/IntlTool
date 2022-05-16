aa470833d7b28c943e8d15055d54c9c20b900ec6
lejunyang <junyang.le@hand-china.com>
2022-05-16 19:32:13 +0800

    fix: webpack配置更新

--- webpack/renderer.webpack.js
+++ webpack/renderer.webpack.js
@@ -1,8 +1,16 @@
+/*
+ * @Author: junyang.le@hand-china.com
+ * @Date: 2022-01-20 10:11:01
+ * @LastEditTime: 2022-01-26 16:32:55
+ * @LastEditors: junyang.le@hand-china.com
+ * @Description: your description
+ * @FilePath: \tool\webpack\renderer.webpack.js
+ */
 module.exports = {
   resolve: {
-    extensions: ['.ts', '.tsx', '.js']
+    extensions: ['.ts', '.tsx', '.js', '.less'],
   },
   module: {
     rules: require('./rules.webpack'),
   },
-}
+};
--- webpack/rules.webpack.js
+++ webpack/rules.webpack.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: junyang.le@hand-china.com
+ * @Date: 2022-01-20 10:11:01
+ * @LastEditTime: 2022-01-28 10:56:08
+ * @LastEditors: junyang.le@hand-china.com
+ * @Description: your description
+ * @FilePath: \tool\webpack\rules.webpack.js
+ */
 module.exports = [
   {
     test: /\.node$/,
@@ -7,7 +15,14 @@ module.exports = [
     test: /\.(m?js|node)$/,
     parser: { amd: false },
     use: {
-      loader: '@marshallofsound/webpack-asset-relocator-loader',
+      /**
+       * 在代码生成那里使用prettier之后下面的loader编译就报错
+       * Error: Module build failed (from ./node_modules/@marshallofsound/webpack-asset-relocator-loader/dist/index.js)
+       * SyntaxError
+       * 在https://github.com/electron-userland/electron-forge/issues/2154找到新的loader，换上
+       */
+      // loader: '@marshallofsound/webpack-asset-relocator-loader',
+      loader: '@timfish/webpack-asset-relocator-loader',
       options: {
         outputAssetBase: 'native_modules',
       },
@@ -17,8 +32,8 @@ module.exports = [
     test: /\.(js|ts|tsx)$/,
     exclude: /node_modules/,
     use: {
-      loader: 'babel-loader'
-    }
+      loader: 'babel-loader',
+    },
   },
   {
     test: /\.(png|jpe?g|gif)$/i,
@@ -26,5 +41,28 @@ module.exports = [
     options: {
       name: '[path][name].[ext]',
     },
-  }
-]
+  },
+  {
+    test: /\.css$/i,
+    use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+  },
+  {
+    test: /\.less$/i,
+    use: [
+      {
+        loader: 'style-loader', // creates style nodes from JS strings
+      },
+      {
+        loader: 'css-loader', // translates CSS into CommonJS
+      },
+      {
+        loader: 'less-loader', // compiles Less to CSS
+        options: {
+          lessOptions: {
+            javascriptEnabled: true, // 没有这个antd-pro编辑报错
+          },
+        },
+      },
+    ],
+  },
+];
