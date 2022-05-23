5dc3459d03a7b5291838b05ef993e0b5ea16abee
lejunyang <junyang.le@hand-china.com>
2022-05-23 23:03:40 +0800

    fix: 更改导出

--- electron/traverse/visitor/index.ts
+++ electron/traverse/visitor/index.ts
@@ -1,12 +1,10 @@
 /*
  * @Author: junyang.le@hand-china.com
  * @Date: 2021-12-24 17:16:51
- * @LastEditTime: 2022-05-20 10:05:25
+ * @LastEditTime: 2022-05-23 23:03:23
  * @LastEditors: junyang.le@hand-china.com
  * @Description: your description
  * @FilePath: \tool\electron\traverse\visitor\index.ts
  */
-import { getIntlTraverseVisitor } from './intlTraverseVisitor';
-import { getChToIntlVisitor } from './chToIntlVisitor';
-
-export { getIntlTraverseVisitor, getChToIntlVisitor };
+export * from './intlTraverseVisitor';
+export * from './chToIntlVisitor';
