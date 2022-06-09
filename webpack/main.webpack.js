57109280882874857b8a7d23089521c8decd7d0e
lejunyang <junyang.le@hand-china.com>
2022-06-09 11:29:11 +0800

    fix: 原先主线程和渲染线程的webpack配置公用一个rules，那里都用了@timfish/webpack-asset-relocator-loader，而该loader在渲染线程打包会导致require is not defined，故删除rules.webpack.js，两个配置文件用不同的loader

--- webpack/main.webpack.js
+++ webpack/main.webpack.js
@@ -1,9 +1,48 @@
+/*
+ * @Author: junyang.le@hand-china.com
+ * @Date: 2022-01-20 10:11:01
+ * @LastEditTime: 2022-06-09 11:28:16
+ * @LastEditors: junyang.le@hand-china.com
+ * @Description: your description
+ * @FilePath: \tool\webpack\main.webpack.js
+ */
+
+// 主线程webpack配置
 module.exports = {
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.js'],
   },
   entry: './electron/main.ts',
   module: {
-    rules: require('./rules.webpack'),
-  }
-}
+    rules: [
+      {
+        test: /\.node$/,
+        use: 'node-loader',
+      },
+      {
+        test: /\.(m?js|node)$/,
+        parser: { amd: false },
+        use: {
+          /**
+           * 在代码生成那里使用prettier之后下面的loader编译就报错
+           * Error: Module build failed (from ./node_modules/@marshallofsound/webpack-asset-relocator-loader/dist/index.js)
+           * SyntaxError
+           * 在https://github.com/electron-userland/electron-forge/issues/2154 找到新的loader，换上
+           */
+          // loader: '@marshallofsound/webpack-asset-relocator-loader',
+          loader: '@timfish/webpack-asset-relocator-loader',
+          options: {
+            outputAssetBase: 'native_modules',
+          },
+        },
+      },
+      {
+        test: /\.(js|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
--- webpack/renderer.webpack.js
+++ webpack/renderer.webpack.js
@@ -1,16 +1,56 @@
 /*
  * @Author: junyang.le@hand-china.com
  * @Date: 2022-01-20 10:11:01
- * @LastEditTime: 2022-01-26 16:32:55
+ * @LastEditTime: 2022-06-09 11:23:30
  * @LastEditors: junyang.le@hand-china.com
  * @Description: your description
  * @FilePath: \tool\webpack\renderer.webpack.js
  */
+
+// 渲染线程webpack配置
 module.exports = {
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.less'],
   },
   module: {
-    rules: require('./rules.webpack'),
+    rules: [
+      {
+        test: /\.(js|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+      },
+      {
+        test: /\.less$/i,
+        use: [
+          {
+            loader: 'style-loader', // creates style nodes from JS strings
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS
+          },
+          {
+            loader: 'less-loader', // compiles Less to CSS
+            options: {
+              lessOptions: {
+                javascriptEnabled: true, // 没有这个antd-pro编辑报错
+              },
+            },
+          },
+        ],
+      },
+    ],
   },
 };
